1fda5ba91e52a27fcfe162e418212dda439ef408
amrendergill <amrender028@gmail.com>
2021-10-09 00:01:14 +0530

    ini

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path=require("path");
+
+const session=require("express-session");
+const app = express();
+const port = process.env.PORT || 3000;
+const{v4:uuidv4}=require("uuid");
+const router=require('./router');
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+
+app.set("view engine", "ejs");
+
+//load static assets
+app.use('/static',express.static(path.join(__dirname,'public')))
+app.use('/assets',express.static(path.join(__dirname,'public/assets')))
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}))
+app.use('/route', router);
+
+//home route
+app.get("/", (req, res) => {
+  res.render("base", { title: "Login system" });
+});
+app.listen(port,()=>{console.log("Listening the server on http://localhost:3000")});
